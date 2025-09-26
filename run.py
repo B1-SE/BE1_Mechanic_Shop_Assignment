028f8a8debe56b4adfa5a73e10b28d50d0eb5dea
B1-SE <bajones2@coastal.edu>
2025-09-25 19:41:32 -0500

    3 files reformatted

--- run.py
+++ run.py
@@ -10,7 +10,8 @@ from app import create_app
 from config import config
 
 # Get the configuration name from the environment variable or default to 'development'
-config_name = os.getenv("FLASK_ENV", "default")  # Ensure a valid config is selected, falling back to default if the env var is invalid
+config_name = os.getenv("FLASK_ENV", "default")
+# Ensure a valid config is selected, falling back to default if the env var is invalid
 app_config = config.get(config_name, config["default"])
 app = create_app(app_config)
 
